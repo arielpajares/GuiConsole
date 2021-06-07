160c53b21969b06a73e4d0fa0a8738dc2c6d3f29
arielpajares <37309592+arielpajares@users.noreply.github.com>
2021-06-07 19:54:30 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+import os
+import time
+import keyboard
+
+
+class Engine():
+	def __init__(self):
+		pass
+
+	def FillRoom(self,width,height,fill):
+		room = []
+		for i in range(height):
+			fillroom = []
+			for j in range(width):
+				fillroom.append(fill)
+			room.append(fillroom)
+
+		return room
+
+	def PrintRoom(self,room):
+		for i in room:
+			printroom = ""
+			for j in i:
+				printroom += j
+
+			print(printroom)
+
+class Gui():
+	def __init__(self,room):
+		pass
+
+# Variables.
+
+KeybordInput = keyboard.read_key()
+engine = Engine()
+room = engine.FillRoom(50,25,"X")
+engine.PrintRoom(room)
+
